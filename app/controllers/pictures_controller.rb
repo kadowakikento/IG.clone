baf45c19ee01c039ff0f50b5161c5b301a3dd891
kento kadowaki <kadowakikento@gmail.com>
2022-06-20 13:34:40 +0900

    ENVを利用したセキュリティ対策

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,16 +1,13 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: %i[ show edit update destroy ]
 
-  # GET /pictures or /pictures.json
   def index
     @pictures = Picture.all
   end
 
-  # GET /pictures/1 or /pictures/1.json
   def show
   end
 
-  # GET /pictures/new
   def new
     if params[:back]
       @picture = Picture.new(picture_params)
@@ -22,19 +19,20 @@ class PicturesController < ApplicationController
   def confirm 
     @picture = Picture.new(picture_params)
     # @picture = current_user.pictures.build(picture_params)
-    render :new if @picture.invalid?
+    # render :new if @picture.invalid?
   end
-  # GET /pictures/1/edit
+
   def edit
   end
 
-  # POST /pictures or /pictures.json
   def create
     @picture = Picture.new(picture_params)
-
+    @picture[:user_id] = session[:user_id]
+    # @picture = current_user.pictures.build(picture_params)
+    
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully created." }
+        format.html { redirect_to picture_url(@picture), notice: "投稿しました！" }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -43,11 +41,10 @@ class PicturesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pictures/1 or /pictures/1.json
   def update
     respond_to do |format|
       if @picture.update(picture_params)
-        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully updated." }
+        format.html { redirect_to picture_url(@picture), notice: "更新しました！" }
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -56,12 +53,11 @@ class PicturesController < ApplicationController
     end
   end
 
-  # DELETE /pictures/1 or /pictures/1.json
   def destroy
     @picture.destroy
 
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: "Picture was successfully destroyed." }
+      format.html { redirect_to pictures_url, notice: "削除しました！" }
       format.json { head :no_content }
     end
   end
